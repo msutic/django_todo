2e06eca342da696bc86d32e626c72a0c54a2eb77
msutic <msuticm@gmail.com>
2021-08-31 23:30:12 +0200

    added form

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 from .models import Task
+from .forms import TaskForm
 # Create your views here.
 
 def index(request):
     queryset = Task.objects.all()
+    form = TaskForm()
     context = {
         'tasks': queryset,
+        'form': form,
     }
     return render(request, 'tasks/list.html', context)
