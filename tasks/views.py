9f835abba3cb6ab150646b4e536eaca16826c29b
msutic <msuticm@gmail.com>
2021-08-31 23:16:33 +0200

    tasks passed within context to template

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Task
 # Create your views here.
 
 def index(request):
-    return render(request, 'tasks/list.html')
+    queryset = Task.objects.all()
+    context = {
+        'tasks': queryset,
+    }
+    return render(request, 'tasks/list.html', context)
